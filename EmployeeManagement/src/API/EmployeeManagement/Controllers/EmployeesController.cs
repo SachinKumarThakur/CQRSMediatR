68995e1ad50772411ba0a05f664fcfff2ac1385b
Sachin Kumar <sachinengineer@hotmail.com>
2021-08-17 11:28:50 +0530

    Commit

--- EmployeeManagement/src/API/EmployeeManagement/Controllers/EmployeesController.cs
+++ EmployeeManagement/src/API/EmployeeManagement/Controllers/EmployeesController.cs
@@ -2,11 +2,8 @@
 using EmployeeManagementLibrary.Models;
 using EmployeeManagementLibrary.Queries;
 using MediatR;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
-using System;
 using System.Collections.Generic;
-using System.Linq;
 using System.Threading.Tasks;
 
 namespace EmployeeManagement.Controllers
